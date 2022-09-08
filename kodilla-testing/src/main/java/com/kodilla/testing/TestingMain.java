4702334615fa0c03dede230bac18ccb29a8ced67
Marcin Kosakowski <kosakowski.m@gmail.com>
2022-09-08 18:45:12 +0200

    6.2 Added class Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //Zadanie: pierwszy test jednostkowy
+
+        Calculator calculator = new Calculator();
+
+        int additionResult = calculator.add(7, 5);
+        int substractionResult = calculator.substract(7, 5);
+
+        if (additionResult == 12) {
+            System.out.println("Addition test ok");
+        } else {
+            System.out.println("Addition test failed");
+        }
+
+        if (substractionResult == 2) {
+            System.out.println("Substraction test ok");
+        } else {
+            System.out.println("Substraction test failed");
+        }
     }
 }
